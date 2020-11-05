b68eba32c8f368ef69f18de323f5a3904c5c7e41
Yajun(Maggie) Wan <59587330+yajunwan@users.noreply.github.com>
2020-11-04 21:32:43 -0800

    9:30pm yolamagi (#2)
    * 8:44pm yolamagi
    
    * modified 3 files in models folder - by yolamagi
    
    Co-authored-by: yajunwan <yolalawan233@gmail.com>

--- src/main/java/com/teamgorm/projectforum/repositories/UserRepository.java
+++ src/main/java/com/teamgorm/projectforum/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.teamgorm.projectforum.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
