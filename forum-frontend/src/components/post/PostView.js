c10a4a4a8b46420f86889da797b48f8a77c77fa6
Jue Fu <rainyforest4869@gmail.com>
2020-11-17 01:31:54 -0800

    #28 Add a 404 page and did Axios refactoring for PostView.js

--- forum-frontend/src/components/post/PostView.js
+++ forum-frontend/src/components/post/PostView.js
@@ -1,44 +1,52 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import AppNavbar from '../common/AppNavbar';
+import { Redirect } from "react-router-dom";
+import axios from 'axios';
 
 class PostView extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {post: {}, isLoading: true};
+        this.state = {post: {}, isLoading: true, redirect: false};
     }
 
     componentDidMount() {
-        // Todo: add check for the id, make sure invalid URLs are directed to 404 page.
-        this.setState({ isLoading: true });
-        if (this.props.match.params.id !== 'new') {
-            fetch(`/api/v1/posts/${this.props.match.params.id}`)
-                .then(response => response.json())
-                .then(data => this.setState({post: data, isLoading: false}));
-        }
+        let curr = this;
+        curr.setState({ isLoading: true });
+        axios
+            .get(`/api/v1/posts/${curr.props.match.params.id}`)
+            .then(res => {
+                curr.setState({post: res.data, isLoading: false});
+            })
+            .catch(err => {
+                curr.setState({redirect: true, isLoading: false});
+            });
 
     }
 
-
-
     render() {
-        const { post, isLoading } = this.state;
+        const { post, isLoading, redirect} = this.state;
         if (isLoading){
             return <p>Loading detailed page....</p>;
         }
+        if (redirect){
+            return <Redirect to="/PageNotFound"/>
+        }
         const title = <h2>{post.title}</h2>;
         const text = <h2>{post.text}</h2>;
-        const comments = post.comments;
-        const commentsList = comments.map(comment =>{
-            return <tr key={comment.id}>
 
-                <td>
-                    {comment.text}<br/>
-                    {comment.user.user_name}
-                </td>
-            </tr>
-        })
+        const commentsList = (post.comments !== null) ?
+            post.comments.map(comment =>{
+                return <tr key={comment.id}>
+                            <td>
+                                {comment.text}<br/>
+                                {comment.user.user_name}
+                            </td>
+                       </tr>
+            }) : <div></div>;
+
+
         return <div>
             <AppNavbar/>
             <div style={{marginLeft:"15em"}}>
