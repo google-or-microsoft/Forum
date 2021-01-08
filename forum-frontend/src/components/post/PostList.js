33549c5e2ec94c555c19563ee7fa4cc59ef0d678
Jason Li <haotian-li@outlook.com>
2021-01-07 22:52:52 -0800

    post list layout css rewrite using tailwindcss

--- forum-frontend/src/components/post/PostList.js
+++ forum-frontend/src/components/post/PostList.js
@@ -4,73 +4,75 @@ import {Link} from 'react-router-dom';
 import {deletePost, getPosts} from "../../services/postService";
 
 const PostList = () => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [posts,
+        setPosts] = useState([]);
+    const [loading,
+        setLoading] = useState(true);
 
     useEffect(() => {
         getPosts()
             .then(posts => setPosts(posts))
-            .finally(() => setLoading(false));
+            . finally(() => setLoading(false));
     });
-
-
 
     const renderPostList = posts.map(post => {
-        return <tr key={post.id}>
-            <td>
-                <Link to={"/posts/" + post.id}>{post.title}</Link><br/>
-            </td>
-            <td>
-                <Link to={"/users/" + post.username}>{post.username}</Link><br/>
-                {post.date}
-            </td>
-            <td>
-
-                <ButtonGroup>
-                    <Button size="small" variant="outlined" color="primary" tag={Link}
-                            href={"/posts/" + post.id + "/edit"}>Edit
+        return (
+            <div className="flex flex-row justify-between my-4">
+                <Link to={"/posts/" + post.id} className="w-1/3">{post.title}</Link>
+                <Link to={"/users/" + post.username} className="w-1/3">{post.username}</Link>
+                <div className="flex flex-row w-1/3">
+                    <Button
+                        size="small"
+                        variant="outlined"
+                        color="primary"
+                        tag={Link}
+                        href={"/posts/" + post.id + "/edit"}
+                        className="mx-1">Edit
                     </Button>
-                    &nbsp;
-                    <Button size="small" variant="outlined" color="secondary" onClick={() => remove(post.id)}>
+                    <Button
+                        size="small"
+                        variant="outlined"
+                        color="secondary"
+                        onClick={() => remove(post.id)}
+                        className="mx-1">
                         Delete
                     </Button>
-                </ButtonGroup>
-            </td>
-        </tr>
-    });
+                </div>
+            </div>
+        )
+    })
 
     const remove = (id) => {
-        deletePost(id)
-            .then(() => {
-                let filteredPosts = [...posts].filter(i => i.id !== id);
-                setPosts(filteredPosts);
-            });
+        deletePost(id).then(() => {
+            let filteredPosts = [...posts].filter(i => i.id !== id);
+            setPosts(filteredPosts);
+        });
     }
 
-    //ToDo: convert each title of the posts
-    //            as a ref link, need to add
-    //            "title" attributes first
+    // ToDo: convert each title of the posts            as a ref link, need to add
+    //       "title" attributes first
     return (
-        <div>
-            {loading ? <p>Loading Content...</p> : <Container fluid>
-                <div className="float-right">
-                    <Button variant="outlined" color="primary" component={Link} to="/posts/new/edit">Add
-                        Post</Button>
-                </div>
-                <h3>My Post List</h3>
-                <Table className="mt-4">
-                    <thead>
-                    <tr>
-                        <th width="20%">Title</th>
-                        <th width="50%">Author</th>
-                        <th width="10%">Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {renderPostList}
-                    </tbody>
-                </Table>
-            </Container>}
+        <div className="mt-2">
+            {loading
+                ? <p>Loading Content...</p>
+                : <Container fluid>
+                    <div className="float-right">
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            component={Link}
+                            to="/posts/new/edit">Add Post</Button>
+                    </div>
+                    <h3>My Post List</h3>
+                    <div className="flex flex-col">
+                        <div id="header" className="flex flex-row justify-between">
+                            <div className="w-1/3 justify-self-center">Title</div>
+                            <div className="w-1/3 justify-self-center">Author</div>
+                            <div className="w-1/3 justify-self-center">Actions</div>
+                        </div>
+                        {renderPostList}
+                    </div>
+                </Container>}
         </div>
     );
 }
