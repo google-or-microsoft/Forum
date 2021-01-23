be0d1e6ae8fbc5964469784380d00c1d03615e09
yolamagi <maggiew820@gmail.com>
2021-01-23 12:18:01 -0800

    actions.js

---
+++ forum-frontend/src/Views/User/actions.js
@@ -0,0 +1,31 @@
+import {login} from "../Auth/api";
+import {SET_PASSWORD, SET_USER, SET_USER_FAILURE, SET_USERNAME, STOP_SET_USER} from "./constants";
+import history from "../../history";
+
+export const setUser = (username,password) =>{
+    return(dispatch) => {
+        login(username,password)
+            .then(data => {
+                dispatch({type:SET_USER,payload:data.username})
+            })
+            .catch(() => dispatch({type:SET_USER_FAILURE}))
+            .finally(() => {
+                dispatch({type:STOP_SET_USER});
+                history.push('/');
+            });
+    }
+}
+
+export const updateUsername = (value) => {
+    return {
+        type: SET_USERNAME,
+        payload: value
+    };
+};
+
+export const updatePassword = (value) => {
+    return {
+        type: SET_PASSWORD,
+        payload: value
+    };
+};
